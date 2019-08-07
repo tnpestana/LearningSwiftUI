49c42f409977c9bb89f8e1ad048ad2fc311f95ef
Tiago Pestana <tnevespestana@gmail.com>
2019-08-07 21:32:57 +0100

    initial commit

---
+++ SwiftUITuto/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITuto
+//
+//  Created by Tiago Pestana on 07/08/2019.
+//  Copyright © 2019 Tiago Pestana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
